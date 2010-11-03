07aa5e29be22bffb429860596a01349ec9d44a8f
Alexandria Marie Wolcott <alyx@spartairc.org>
2010-11-03 00:29:39 -0500

    Finally updated keldair.pl for libkeldair v2

--- bin/keldair.pl
+++ bin/keldair.pl
@@ -17,40 +17,7 @@ use Keldair qw(config);
 
 my $rawlog = File::Data->new("$Bin/../var/raw.log");
 
-our $SETTINGS = Config::JSON->new("$Bin/../etc/keldair.conf")
-  or die("Cannot open config file!\n");
-
-my $modref = $SETTINGS->get("modules");
-
-our @modules = @$modref;
-
-foreach my $mod (@modules) {
-    eval { load $mod; };
-    eval { $mod->_modinit; };
-}
-
-my $host = $SETTINGS->get("server/host");
-my $port = $SETTINGS->get("server/port");
-
-my $ssl = $SETTINGS->get("server/ssl");
-
-our $sock;
-
-if ($ssl =~ /^y.*/) {
-	require IO::Socket::SSL;
-	$sock = IO::Socket::SSL->new(
-		Proto	 => "tcp",
-		PeerAddr => $host,
-		PeerPort => $port,
-	) or die("Connection failed to $host: $!\n");
-}
-else {
-    $sock = IO::Socket::INET->new(
-        Proto    => "tcp",
-        PeerAddr => $host,
-        PeerPort => $port,
-    ) or die("Connection failed to $host. \n");
-}
+Keldair->new("$Bin/../etc/keldair.conf");
 
 my $fork = $SETTINGS->get("debug/fork");
 
@@ -63,74 +30,5 @@ if ( $fork =~ /^yes|y|1/i ) {
     unless ( $pid == 0 ) {
         writepid($pid);
         exit;
-    }
-}
-my (
-    $line,    $nickname,  $command,    $mtext, $hostmask,
-    $channel, $firstword, @spacesplit, @words
-);
-
-my ( $user, $real, $nick ) = (
-    $SETTINGS->get("keldair/user"),
-    $SETTINGS->get("keldair/real"),
-    $SETTINGS->get("keldair/nick")
-);
-
-Keldair::connect( $user, $real, $nick );
-
-# Ok, I do believe connecting is important, eh? :P
-
-IRC: while ( $line = <$sock> ) {
-
-# First, since this is a loop, we undefine all the special and REALLY IMPORTANT variables!
-    undef $nickname;
-    undef $command;
-    undef $mtext;
-    undef $hostmask;
-
-    # Mkay, now let's kill off those \r\n's at the end of $line.
-    chomp($line);
-    chomp($line);
-
-    # Hey, let's print the line too!
-    print( $line. "\r\n" );
-
-    # Hell, why not? Let's log it too, for teh lulz!
-    #$rawlog->append(time." ".$line."\n");
-
-    # Now, time to /extract/ those there variables!
-    $hostmask = substr( $line, index( $line, ":" ) );
-    $mtext = substr( $line, index( $line, ":", index( $line, ":" ) + 1 ) + 1 );
-    ( $hostmask, $command ) =
-      split( " ", substr( $line, index( $line, ":" ) + 1 ) );
-    ( $nickname, undef ) = split( "!", $hostmask );
-    @spacesplit = split( " ", $line );
-    $channel = $spacesplit[2];
-
-    # Now, time for some commandish stuff!
-
-    my $handler = 'handle_' . lc($command);
-
-    foreach my $cmd (@modules) {
-        eval { $cmd->$handler( $hostmask, $channel, $mtext, $line ); };
-    }
-    
-    if ($command eq 'PRIVMSG') {
-		my $cmdchar = config('cmdchar');
-		if ($mtext =~ /^$cmdchar/) {
-			my $text = substr($mtext, length($cmdchar));
-			my @parv = split(' ', $text);
-			my $handler = 'command_' . lc($parv[0]);
-			my (%user, $garbage);
-			($user{'nick'}, $garbage) = split('!', $hostmask);
-			($user{'ident'},$user{'host'}) = split('@', $garbage);
-			foreach my $cmd (@modules) {
-				eval { $cmd->$handler( @parv, $channel, %user, $mtext ); };
-			}
-		}
-	}
-
-    if ( $line =~ /^PING :/ ) {
-        Keldair::snd( "PONG :" . substr( $line, index( $line, ":" ) + 1 ) );
     }
 }
