cc2125f5747153c6da16baeb7c69d9f3ea9a6c99
Alexandria Marie Wolcott <alyx@spartairc.org>
2010-11-15 19:04:33 -0600

    What

--- lib/Keldair.pm
+++ lib/Keldair.pm
@@ -8,25 +8,28 @@ package Keldair;
 
 use strict;
 use warnings;
-use diagnostics;
+use diagnostics -verbose;
+use Carp qw(cluck croak);
 use IO::Socket;
 use Exporter 'import';
 use Module::Load;
 use Config::JSON;
+use FindBin qw($Bin);
+use lib "$Bin/../lib";
 use constant {
     VERSIONSTRING => '2.0.0',
     VERSION       => 2,
     SUBVERSION    => 0,
     REVISION      => 0,
-    RELEASESTAGE  => '',
-    RELEASE       => ''
+    RELEASESTAGE  => ' ',
+    RELEASE       => ' '
 };
 
 # Note for future maintainers:
 # VERSIONSTRING = Keldair::VERSION.'.'.Keldair::SUBVERSION.'.'.Keldair::REVISION.'-'.Keldair::RELEASESTAGE.Keldair::RELEASE;
 
 @Keldair::EXPORT_OK =
-qw(modlist modload act ban config ctcp kick kill mode msg notice oper snd);
+  qw(modlist modload act ban config ctcp kick kill mode msg notice oper snd);
 
 our ( @modules, $sock, $SETTINGS );
 
@@ -45,52 +48,62 @@ sub new {
     Keldair::_connect( config('server/host'), config('server/port') );
 }
 
-
 sub _loop {
     my (
-        $line,    $nickname,  $command,    $mtext, $hostmask,
-        $channel, $firstword, @spacesplit, @words
+        $line,     $nickname, $command,   $mtext,
+        $hostmask, $channel,  $firstword, @spacesplit,
+        @words,    $origin,   $target,    $params
     );
     while ( $line = <$sock> ) {
 
-        undef $nickname;
-        undef $command;
-        undef $mtext;
-        undef $hostmask;
-
-        chomp($line);
-        chomp($line);
+        $line =~ s/\r\n//x;
 
         # Hey, let's print the line too!
-        print( $line. "\r\n" ) if config('debug/verbose') == 1;
+        print( '>> ' . $line . "\r\n" );
 
-        $hostmask = substr( $line, index( $line, ":" ) );
-        $mtext =
-        substr( $line, index( $line, ":", index( $line, ":" ) + 1 ) + 1 );
-        ( $hostmask, $command ) =
-        split( " ", substr( $line, index( $line, ":" ) + 1 ) );
-        ( $nickname, undef ) = split( "!", $hostmask );
-        @spacesplit = split( " ", $line );
-        $channel = $spacesplit[2];
+        if ( $line =~
+            /^(?:\:([^\s]+)\s)?(\w+)\s(?:([^\s\:]+)\s)?(?:\:?(.*))?$/x )
+        {
+            $origin  = $1;
+            $command = $2;
+            $target  = $3;
+            $params  = $4;
+                #if ( $origin =~ /(.*)!(.*)\@(.*)/x ) {
+                 #   $origin = {
+                  #      'hostmask' => $origin,
+                   #     'nick'     => $1,
+                    #    'user'     => $2,
+                     #   'host'     => $3
+                    #};
+               # }
+        }
+        else {
+            ( $command, $params ) = split( ' ', $line );
+        }
+
+     #        $hostmask = substr( $line, index( $line, ":" ) );
+     #        $mtext =
+     #        substr( $line, index( $line, ":", index( $line, ":" ) + 1 ) + 1 );
+     #        ( $hostmask, $command ) =
+     #        split( " ", substr( $line, index( $line, ":" ) + 1 ) );
+     #        ( $nickname, undef ) = split( "!", $hostmask );
+     #        @spacesplit = split( " ", $line );
+     #        $channel = $spacesplit[2];
 
         my $handler = 'handle_' . lc($command);
 
         foreach my $cmd (@modules) {
-            eval { $cmd->$handler( $hostmask, $channel, $mtext, $line ); };
-            print("Error: $!\n") if $!;
+            eval { $cmd->$handler( $origin, $target, $params, $line ); };
         }
 
         if ( $command eq 'PRIVMSG' ) {
             my $cmdchar = config('cmdchar');
-            if ( $mtext =~ /^$cmdchar/ ) {
-                my $text = substr( $mtext, length($cmdchar) );
+            if ( $mtext =~ /^$cmdchar/ix ) {
+                my $text = substr( $params, length($cmdchar) );
                 my @parv = split( ' ', $text );
                 my $handler = 'command_' . lc( $parv[0] );
-                my ( %user, $garbage );
-                ( $user{'nick'}, $garbage ) = split( '!', $hostmask );
-                ( $user{'ident'}, $user{'host'} ) = split( '@', $garbage );
                 foreach my $cmd (@modules) {
-                    eval { $cmd->$handler( @parv, $channel, %user, $mtext ); };
+                    eval { $cmd->$handler( @parv, $target, $origin, $params ); };
                 }
             }
         }
@@ -100,18 +113,20 @@ sub _loop {
             }
         }
 
-        if ( $line =~ /^PING :/ ) {
+        if ( $line =~ /^PING :/x ) {
             snd( "PONG :" . substr( $line, index( $line, ":" ) + 1 ) );
         }
+
     }
     foreach my $cmd (@modules) {
         eval { $cmd->on_disconnect; };
     }
+    return 1;
 }
 
 sub _connect {
     my ( $host, $port ) = @_;
-    if ( Keldair::config('server/ssl') =~ /^y.*/ ) {
+    if ( Keldair::config('server/ssl') =~ /^y.*/ix ) {
         require IO::Socket::SSL;
         $sock = IO::Socket::SSL->new(
             Proto    => "tcp",
@@ -131,16 +146,23 @@ sub _connect {
         Keldair::config('keldair/real'),
         Keldair::config('keldair/nick')
     );
-    Keldair::_loop;
+    _loop
+      or croak(
+"Missing Keldair::_loop, report this to http://github.com/keldair/keldair!"
+      ) and return 0;
 }
 
 sub connect {
     my ( $ident, $gecos, $nick ) = @_;
     my $pass = config('server/pass');
 
+    foreach my $cmd (@modules) {
+        eval { $cmd->on_preconnect; };
+    }
     snd("PASS $pass") if defined($pass);
     snd("USER $ident * * :$gecos");
     snd("NICK $nick");
+    return 1;
 }
 
 #---------------------------------------------
@@ -148,22 +170,25 @@ sub connect {
 #---------------------------------------------
 
 sub modload {
-    my ($mod) = $_[0];
-    eval { load $mod; };
-    eval { $mod->_modinit; };
+    my ($mod) = @_;
+    #eval { load $mod; }      or cluck("Loading $mod failed\n") and return 0;
+    eval { load $mod; }; print("Err: $!\n") if $!;
+    eval { $mod->_modinit; }; print("Err: $!\n") if $1;
+    #eval { $mod->_modinit; } or cluck("Missing _modinit!")      and return 0;
     push( @modules, $mod );
+    return 1;
 }
 
 #sub modreload {
-    #my ($mod) = $_[0];
-    #modunload($mod);
-    #modload($mod);
+#my ($mod) = $_[0];
+#modunload($mod);
+#modload($mod);
 #}
 
 #sub modunload {
-    #my ($module) = $_[0];
-    #no $module;
-    #@modules = grep{!/^$module$/};
+#my ($module) = $_[0];
+#no $module;
+#@modules = grep{!/^$module$/};
 #}
 
 sub modlist {
@@ -183,77 +208,86 @@ sub config {
 sub snd {
     my ($text) = @_;
     chomp($text);
-    print("SEND: $text\r\n") if config('debug/verbose') == 1;
+    print("<< $text\r\n") if config('debug/verbose') == 1;
     send( $sock, $text . "\r\n", 0 );
+    return $text;
 }
 
 sub msg {
     my ( $target, $text ) = @_;
     snd( "PRIVMSG " . $target . " :" . $text );
+    return 1;
 }
 
 sub notice {
     my ( $target, $text ) = @_;
     snd( "NOTICE " . $target . " :" . $text );
+    return 1;
 }
 
 sub ctcp {
     my ( $target, $text ) = @_;
     snd( "PRIVMSG " . $target . " :\001" . $text . "\001" );
+    return 1;
 }
 
 sub act {
     my ( $target, $text ) = @_;
     snd( "PRIVMSG " . $target . " :\001ACTION " . $text . "\001" );
+    return 1;
 }
 
 sub oper {
     my ( $name, $pass ) = @_;
-    snd( 'OPER ' . $name . ' ' . $pass );
+    snd("OPER $name $pass");
+    return 1;
 }
 
-sub kill {
+sub userkill {
     my ( $target, $msg ) = @_;
     snd("KILL $target :$msg");
+    return 1;
 }
 
 sub ban {
     my ( $channel, $host ) = @_;
     snd("MODE $channel +b $host");
+    return 1;
 }
 
 sub kick {
     my ( $channel, $target, $reason ) = @_;
     snd("KICK $channel $target :$reason");
+    return 1;
 }
 
 sub mode {
     my ( $target, $modes ) = @_;
     snd("MODE $target $modes");
-}
-
-sub kill {
-    my ( $target, $reason ) = @_;
-    snd("KILL $target :$reason");
+    return 1;
 }
 
 sub topic {
     my ( $channel, $topic ) = @_;
     snd("TOPIC $channel :$topic");
+    return 1;
 }
 
 sub away {
     my $reason = @_;
     snd("AWAY :$reason");
+    return 1;
 }
 
 sub back {
     snd("AWAY");
+    return 1;
 }
 
-sub join {
+sub cjoin {
     my $channel = @_;
     snd("JOIN $channel");
+    return 1;
 }
 
-1;
+1337*(22/7) <= 9001;
