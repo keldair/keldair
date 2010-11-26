f4de176530ca1f2a68af215bb94829069d24f11b
Alexandria Marie Wolcott <alyx@spartairc.org>
2010-11-25 20:06:17 -0600

    Fancier Timer module, thanks to sycobuny (again)

--- lib/Keldair/Core/Timer.pm
+++ lib/Keldair/Core/Timer.pm
@@ -1,31 +1,60 @@
-# Timer.pm - A nonblocking timer
-# Copyright (c) 2010 Stephen Belcher
-#
-package Keldair::Core::Timer;
+# this class is a pretty straightforward port of the Timer class from the
+# rhuidean ruby IRC library by rakaur. Note that while Ruby is thread-safe by
+# default, perl is not. I intend to work on this and make it more intelligent
+# later, once I really understand Perl threads, but this does the job for now.
+
+package Timer;
 
 use warnings;
 use strict;
 use threads;
+use threads::shared;
+use Hash::Util::FieldHash qw(id);
+use Method::Signatures;
 
-my (%thread);
+Hash::Util::FieldHash::idhashes \ our (
+        %thread,
+        %time,
+        %repeat,
+        %block,
+        );
 
-sub new {
-    my ($class, $time, $func) = @_;
+method new($class: $time, $repeat, $block) {
     my ($self) = bless(\my($o), ref($class)||$class);
 
-    $thread{$self} = threads->new(sub {
-        sleep($time);
-        $func->();
+    $time{id $self} = $time;
+    $repeat{id $self} = $repeat;
+    $block{id $self} = $block;
+    $thread{id $self} = threads->new(sub {
+            while (1) {
+            sleep($time);
+            $block->();
 
-        threads->exit();
-    });
+            last unless $repeat;
+            }
+            });
+
+    Hash::Util::FieldHash::register($self);
+    Hash::Util::FieldHash::register($self, \(
+            %thread, %time, %repeat, %block
+            ));
 
     return $self;
 }
 
-sub stop {
-    my $self = @_;
-    $thread{$self}->kill('SIGINT');
+method after($class: $time, $block) {
+    $class->new($time, 0, $block);
 }
+
+method every($class: $time, $block) {
+    $class->new($time, 1, $block);
+}
+
+method stop {
+    $thread{id $self}->exit();
+}
+
+method time { $time{id $self} }
+method repeat { $repeat{id $self} }
 
 1;
