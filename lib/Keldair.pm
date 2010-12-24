ad59e22ecf0855fb7885cd9f8f8d239f82348a66
Alexandria Marie Wolcott <alyx@woomoo.org>
2010-12-24 05:08:36 -0600

    <3 gg=G

--- lib/Keldair.pm
+++ lib/Keldair.pm
@@ -19,162 +19,162 @@ use FindBin qw($Bin);
 use lib "$Bin/../lib";
 use Keldair::Core::Parser qw(parse_irc);
 use constant {
-    VERSIONSTRING => '2.2.1',
-    VERSION       => 2,
-    SUBVERSION    => 0,
-    REVISION      => 0,
-    RELEASESTAGE  => ' ',
-    RELEASE       => ' '
+	VERSIONSTRING => '2.2.1',
+	VERSION       => 2,
+	SUBVERSION    => 0,
+	REVISION      => 0,
+	RELEASESTAGE  => ' ',
+	RELEASE       => ' '
 };
 
 # Note for future maintainers:
 # VERSIONSTRING = Keldair::VERSION.'.'.Keldair::SUBVERSION.'.'.Keldair::REVISION.'-'.Keldair::RELEASESTAGE.Keldair::RELEASE;
 
 @Keldair::EXPORT_OK =
-  qw(act away back ban cjoin config connect ctcp kick mode modlist modload msg notice oper snd topic userkill cpart);
+qw(act away back ban cjoin config connect ctcp kick mode modlist modload msg notice oper snd topic userkill cpart);
 
 our ( @modules, $sock, $SETTINGS, $me );
 
 # Remember to allow anything you want to call in modules.
 
 sub new {
-    my $self = shift;
-    my ($config) = $_[0];
-    $SETTINGS = Config::JSON->new($config)
-      or die("Cannot open config file!\n");
-    my $modref = $SETTINGS->get("modules");
-    my @tmp    = @$modref;
-    foreach my $mod (@tmp) {
-        Keldair::modload($mod);
-    }
-    push( @modules, 'main' );
-    foreach my $mod (@modules) {
-        eval { $mod->on_startup; };
-    }
-    _connect( config('server/host'), config('server/port') ) or
-        croak("Error connecting to server: $!\n");
-    return 1;
+	my $self = shift;
+	my ($config) = $_[0];
+	$SETTINGS = Config::JSON->new($config)
+		or die("Cannot open config file!\n");
+	my $modref = $SETTINGS->get("modules");
+	my @tmp    = @$modref;
+	foreach my $mod (@tmp) {
+		Keldair::modload($mod);
+	}
+	push( @modules, 'main' );
+	foreach my $mod (@modules) {
+		eval { $mod->on_startup; };
+	}
+	_connect( config('server/host'), config('server/port') ) or
+	croak("Error connecting to server: $!\n");
+	return 1;
 }
 
 sub _loop {
-    my ($line);
-    while ( $line = <$sock> ) {
-      my $verbose = config('debug/verbose');
-      $verbose ||= 'no';
-        print ">> ".$line if $verbose =~ (m/^(y.*|on|1|t.*)$/i);
-        $line =~ s/\r\n//x;
+	my ($line);
+	while ( $line = <$sock> ) {
+		my $verbose = config('debug/verbose');
+		$verbose ||= 'no';
+		print ">> ".$line if $verbose =~ (m/^(y.*|on|1|t.*)$/i);
+		$line =~ s/\r\n//x;
 
-        my $event = parse_irc($line);
+		my $event = parse_irc($line);
 
-        my $handler = 'handle_' . lc( $event->{command} );
+		my $handler = 'handle_' . lc( $event->{command} );
 
-        foreach my $cmd (@modules) {
-            eval {
-                $cmd->$handler(
-                    $event->{origin}, $event->{target},
-                    $event->{params}, $line
-                );
-            };
-        }
+		foreach my $cmd (@modules) {
+			eval {
+				$cmd->$handler(
+					$event->{origin}, $event->{target},
+					$event->{params}, $line
+				);
+			};
+		}
 
-        if ( $event->{command} eq '001' ) {
-            foreach my $cmd (@modules) {
-                eval { $cmd->on_connect; };
-            }
-        }
-
-	if ( $event->{command} eq 'PRIVMSG' ) {
-		my @args = split(' ', $event->{params});
-		if ($line =~ m/\001/x) { # This is a ctcp
-			$event->{params} =~ s/\001//g;
-			my ($ctcp, undef) = split(' ', $event->{params});
-			my $handler = 'ctcp_'.lc($ctcp);
+		if ( $event->{command} eq '001' ) {
 			foreach my $cmd (@modules) {
-				eval {
-					$cmd->$handler($event, $line);
+				eval { $cmd->on_connect; };
+			}
+		}
+
+		if ( $event->{command} eq 'PRIVMSG' ) {
+			my @args = split(' ', $event->{params});
+			if ($line =~ m/\001/x) { # This is a ctcp
+				$event->{params} =~ s/\001//g;
+				my ($ctcp, undef) = split(' ', $event->{params});
+				my $handler = 'ctcp_'.lc($ctcp);
+				foreach my $cmd (@modules) {
+					eval {
+						$cmd->$handler($event, $line);
+					}
+				}
+			}
+			if (substr($args[0], 0, 1) eq config('cmdchar')) {
+				my $handler = 'cmd_'.lc($event->{command});
+				foreach my $cmd (@modules) {
+					eval {
+						$cmd->$handler($event->{origin}, $event->{target}, $event->{params}, $line);
+					};
 				}
 			}
 		}
-		if (substr($args[0], 0, 1) eq config('cmdchar')) {
-			my $handler = 'cmd_'.lc($event->{command});
-			foreach my $cmd (@modules) {
-				eval {
-					$cmd->$handler($event->{origin}, $event->{target}, $event->{params}, $line);
-				};
-			}
+
+		elsif ( $event->{command} eq 'PING') {
+			#snd( "PONG :" . substr( $line, index( $line, ":" ) + 1 ) );
+			snd( "PONG :$event->{params}" );
 		}
+
 	}
-
-        elsif ( $event->{command} eq 'PING') {
-            #snd( "PONG :" . substr( $line, index( $line, ":" ) + 1 ) );
-            snd( "PONG :$event->{params}" );
-        }
-
-    }
-    foreach my $cmd (@modules) {
-        eval { $cmd->on_disconnect; };
-    }
-    return 1;
+	foreach my $cmd (@modules) {
+		eval { $cmd->on_disconnect; };
+	}
+	return 1;
 }
 
 sub _connect {
-    #$sock = Keldair::Core::Connect->init() or croak("Connection failed: $!");
+	#$sock = Keldair::Core::Connect->init() or croak("Connection failed: $!");
 
-    my %connecthash = (
-        'Proto'    => "tcp",
-        'PeerAddr' => Keldair::config('server/host'),
-        'PeerPort' => Keldair::config('server/port'),
-        'Timeout'  => 30
-    );
+	my %connecthash = (
+		'Proto'    => "tcp",
+		'PeerAddr' => Keldair::config('server/host'),
+		'PeerPort' => Keldair::config('server/port'),
+		'Timeout'  => 30
+	);
 
-    if ( config('server/ssl') =~ /^(y.*|on|1|t.*)$/i ) {
-        eval { require IO::Socket::SSL; } or croak("Missing IO::Socket::SSL");
+	if ( config('server/ssl') =~ /^(y.*|on|1|t.*)$/i ) {
+		eval { require IO::Socket::SSL; } or croak("Missing IO::Socket::SSL");
 
-        #if ( config( 'ssl/certfp' =~ /^(y.*|on|1|t.*)$/i ) ) {
-            #$connecthash{'SSL_cert_file'} = config('ssl/certfp/filename');
-            #if ( config('ssl/certfp/passwd') ) {
-                #$connecthash{'SSL_passwd_cb'} =
-                  #sub { return config('ssl/certfp/passwd'); }
-            #}
-        #}
-        $sock = IO::Socket::SSL->new(%connecthash)
-            or
-          croak( "Connection failed to " . config('server/host') . ": $!\n" );
-    }
+		#if ( config( 'ssl/certfp' =~ /^(y.*|on|1|t.*)$/i ) ) {
+		#$connecthash{'SSL_cert_file'} = config('ssl/certfp/filename');
+		#if ( config('ssl/certfp/passwd') ) {
+		#$connecthash{'SSL_passwd_cb'} =
+		#sub { return config('ssl/certfp/passwd'); }
+		#}
+		#}
+		$sock = IO::Socket::SSL->new(%connecthash)
+			or
+		croak( "Connection failed to " . config('server/host') . ": $!\n" );
+	}
 
-    else {
-        $sock = IO::Socket::INET->new(%connecthash)
-          or
-          croak( "Connection failed to " . config('server/host') . ": $!\n" );
-    }
+	else {
+		$sock = IO::Socket::INET->new(%connecthash)
+			or
+		croak( "Connection failed to " . config('server/host') . ": $!\n" );
+	}
 
 
-    Keldair::connect(
-        config('keldair/user'),
-        config('keldair/real'),
-        config('keldair/nick')
-    );
-    _loop
-      or croak(
-"Missing Keldair::_loop, report this to http://github.com/keldair/keldair!"
-      ) and return 0;
+	Keldair::connect(
+		config('keldair/user'),
+		config('keldair/real'),
+		config('keldair/nick')
+	);
+	_loop
+		or croak(
+		"Missing Keldair::_loop, report this to http://github.com/keldair/keldair!"
+	) and return 0;
 }
 
 sub connect {
-    my ( $ident, $gecos, $nick ) = @_;
-    my $pass = config('server/pass');
+	my ( $ident, $gecos, $nick ) = @_;
+	my $pass = config('server/pass');
 
-    foreach my $cmd (@modules) {
-        eval { $cmd->on_preconnect; };
-    }
-    snd("PASS $pass") if defined($pass);
-    snd(    "USER $ident "
-          . hostname . " "
-          . config('server/host')
-          . " :$gecos" );
-    snd("NICK $nick");
-    $me = $nick;
-    return 1;
+	foreach my $cmd (@modules) {
+		eval { $cmd->on_preconnect; };
+	}
+	snd("PASS $pass") if defined($pass);
+	snd(    "USER $ident "
+		. hostname . " "
+		. config('server/host')
+		. " :$gecos" );
+	snd("NICK $nick");
+	$me = $nick;
+	return 1;
 }
 
 #---------------------------------------------
@@ -182,15 +182,15 @@ sub connect {
 #---------------------------------------------
 
 sub modload {
-    my ($mod) = @_;
-    eval { load $mod; };
-    print("$mod failed: $!\n") if $!;
-    eval { $mod->modinit; };
-    print( "$mod" . "::modinit failed: $!\n" ) if $!;
+	my ($mod) = @_;
+	eval { load $mod; };
+	print("$mod failed: $!\n") if $!;
+	eval { $mod->modinit; };
+	print( "$mod" . "::modinit failed: $!\n" ) if $!;
 
-    # eval { $mod->modinit; } or cluck("Missing modinit!")      and return 0;
-    push( @modules, $mod );
-    return 1;
+	# eval { $mod->modinit; } or cluck("Missing modinit!")      and return 0;
+	push( @modules, $mod );
+	return 1;
 }
 
 #sub modreload {
@@ -206,13 +206,13 @@ sub modload {
 #}
 
 sub modlist {
-    return @modules;
+	return @modules;
 }
 
 sub config {
-    my ($value) = @_;
-    my $setting = $SETTINGS->get($value);
-    return $setting;
+	my ($value) = @_;
+	my $setting = $SETTINGS->get($value);
+	return $setting;
 }
 
 #------------------------
@@ -220,116 +220,116 @@ sub config {
 #------------------------
 
 sub snd {
-    my ($text) = @_;
-    chomp($text);
-    my $verbose = config('debug/verbose');
-    $verbose ||= 'no';
-    if ($verbose =~ /^(y.*|on|1|t.*)$/i)
-     {
-       print("<< $text\r\n");
-     }
-    send( $sock, $text . "\r\n", 0 );
-    return $text;
+	my ($text) = @_;
+	chomp($text);
+	my $verbose = config('debug/verbose');
+	$verbose ||= 'no';
+	if ($verbose =~ /^(y.*|on|1|t.*)$/i)
+	{
+		print("<< $text\r\n");
+	}
+	send( $sock, $text . "\r\n", 0 );
+	return $text;
 }
 
 sub msg {
-    my ( $target, $text ) = @_;
-    snd("PRIVMSG $target :$text");
-    return 1;
+	my ( $target, $text ) = @_;
+	snd("PRIVMSG $target :$text");
+	return 1;
 }
 
 sub notice {
-    my ( $target, $text ) = @_;
-    snd("NOTICE $target :$text");
-    return 1;
+	my ( $target, $text ) = @_;
+	snd("NOTICE $target :$text");
+	return 1;
 }
 
 sub ctcp {
-    my ( $target, $text ) = @_;
-    snd( "PRIVMSG $target :\001$text\001");
-    return 1;
+	my ( $target, $text ) = @_;
+	snd( "PRIVMSG $target :\001$text\001");
+	return 1;
 }
 
 sub ctcpreply {
-    my ( $target, $text ) = @_;
-    snd("NOTICE $target :\001$text\001");
-    return 1;
+	my ( $target, $text ) = @_;
+	snd("NOTICE $target :\001$text\001");
+	return 1;
 }
 
 sub act {
-    my ( $target, $text ) = @_;
-    snd( "PRIVMSG $target :\001ACTION $text\001" );
-    return 1;
+	my ( $target, $text ) = @_;
+	snd( "PRIVMSG $target :\001ACTION $text\001" );
+	return 1;
 }
 
 sub oper {
-    my ( $name, $pass ) = @_;
-    snd("OPER $name $pass");
-    return 1;
+	my ( $name, $pass ) = @_;
+	snd("OPER $name $pass");
+	return 1;
 }
 
 sub userkill {
-    my ( $target, $msg ) = @_;
-    snd("KILL $target :$msg");
-    return 1;
+	my ( $target, $msg ) = @_;
+	snd("KILL $target :$msg");
+	return 1;
 }
 
 sub ban {
-    my ( $channel, $host ) = @_;
-    snd("MODE $channel +b $host");
-    return 1;
+	my ( $channel, $host ) = @_;
+	snd("MODE $channel +b $host");
+	return 1;
 }
 
 sub unban {
-    my ( $channel, $host ) = @_;
-    snd("MODE $channel -b $host");
-    return 1;
+	my ( $channel, $host ) = @_;
+	snd("MODE $channel -b $host");
+	return 1;
 }
 
 sub kick {
-    my ( $channel, $target, $reason ) = @_;
-    snd("KICK $channel $target :".($reason ? $reason : "No reason given."));
-    return 1;
+	my ( $channel, $target, $reason ) = @_;
+	snd("KICK $channel $target :".($reason ? $reason : "No reason given."));
+	return 1;
 }
 
 sub mode {
-    my ( $target, $modes ) = @_;
-    snd("MODE $target $modes");
-    return 1;
+	my ( $target, $modes ) = @_;
+	snd("MODE $target $modes");
+	return 1;
 }
 
 sub topic {
-    my ( $channel, $topic ) = @_;
-    snd("TOPIC $channel :$topic");
-    return 1;
+	my ( $channel, $topic ) = @_;
+	snd("TOPIC $channel :$topic");
+	return 1;
 }
 
 sub away {
-    my $reason = @_;
-    snd("AWAY :$reason");
-    return 1;
+	my $reason = @_;
+	snd("AWAY :$reason");
+	return 1;
 }
 
 sub back {
-    snd("AWAY");
-    return 1;
+	snd("AWAY");
+	return 1;
 }
 
 sub cjoin {
-    my $channel = @_;
-    snd("JOIN $channel");
-    return 1;
+	my $channel = @_;
+	snd("JOIN $channel");
+	return 1;
 }
 
 sub cpart {
-    my ($channel,$reason) = @_;
-    snd("PART ".$channel." :".$reason);
+	my ($channel,$reason) = @_;
+	snd("PART ".$channel." :".$reason);
 }
 
 sub nick {
-    my $nick = @_;
-    snd("NICK $nick");
-    return $nick;
+	my $nick = @_;
+	snd("NICK $nick");
+	return $nick;
 }
 
 
